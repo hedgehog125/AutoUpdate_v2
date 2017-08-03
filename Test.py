c0abaa338f953e6ac0ca9d9dd52061c677faafd5
Nico Clack <nico@bn7.net>
2017-08-03 17:31:13 +0100

    Test.py now actually works. :P

--- Test.py
+++ Test.py
@@ -1,3 +1,3 @@
 import AutoUpdate
-AutoUpdate.database = "https://raw.githubusercontent.com/hedgehog125/AutoUpdate_v2_Database/master/"
+AutoUpdate.database = "https://raw.githubusercontent.com/hedgehog125/AutoUpdate_v2_Database/master/database/"
 AutoUpdate.init()
